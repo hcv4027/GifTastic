2bf18c89ed881336c0220e0d90c7a48f2439cf41
HCV Bootcamp <hcv4027@gmail.com>
2018-11-08 01:48:45 -0500

    Updated CSS, added images, commented code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,79 +1,104 @@
-// array of animals. used to generate buttons dynamically.
-var topics = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capbara", "teacup pig", "serval", "salamander", "frog"];
+// An array of animals used for generated buttons.
+var topics = ["Bird", "Cat", "Camel", "Chicken",
+              "Deer", "Dog", "Donkey", "Duck",
+              "Ferret", "Frog", "Gazelle", "Goat", 
+              "Hamster", "Horse", "Llama", "Ox",
+              "Rabbit", "Sheep", "Skunk",  "Swan", 
+              "Turkey",  "Turtle", "Water Buffalo"
+              ];
+
+//Individual pieces of information needed for ajax query
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
-var apiKey = "IUVeDO5tMqDFSLwLqPojoTz1CQD7bug7";
+var apiKey = "aJ2r8kvvJRFG4mawIZyH4fOw3LgX015T";
 var searchQty = 10;
-// function to create button elements and display them on the page
-function buttonMaker() {
+
+// This function creates button elements that are displayed on the page.
+function buttonGenerator() {
   $("#animal-buttons").empty();
-  for (i = 0; i <  topics.length; i++) {
+  for (i = 0; i <  topics.length; i++) 
+  {
     var emptyButton = $("<button>");
-    // use bootstrap classes for UI. animal-button class used to target later with click event
-    $(emptyButton).addClass("btn btn-primary m-1 animal-button");
-    // set data attribute equal to the string from the array in case i need to reference later
+    // Adding bootstrap classes to organize elements in the UI.
+    $(emptyButton).addClass("btn btn-info m-1 animal-button");
+
+    // Setting a data attribute equal to the string of the current element from the array.
     $(emptyButton).attr("id", topics[i]);
     $(emptyButton).html(topics[i]);
     $("#animal-buttons").append(emptyButton);
   }
 };
-// global function to switch between the animated images and the still images
-function motionMaker() {
-  // if the image the user clicks on is currently in the "still" state, switch to "motion"
+
+// This function toggles between the animated image and the still image.
+function motionToggle() 
+{
+  // if the image clicked is currently still, toggle it to animated.
   if ($(this).attr("current-state") === "img-still") {
     $(this).attr("current-state", "img-motion");
     $(this).attr("src", $(this).attr("img-motion"));
-  // if the image the users clicks on is currently in the "motion" state, switch to "still"
-  } else {
+  } 
+  // if the image clicked is currently animated, toggle it to still.
+  else 
+  {
     $(this).attr("current-state", "img-still");
     $(this).attr("src", $(this).attr("img-still"));
   }
 };
+
 function cardMaker() {
-  // clear any previous elements that were generated
+  // Removes any elements that were generated
   $("#animals").empty();
-  // ajax call based on the paramters set at the top of the file
+
+  // Ajax query call including the parameters created and set above.
   $.ajax({
     url: queryURL + $(this).attr("id") + "&api_key=" + apiKey + "&limit=" + searchQty,
     method: "GET"
   }).then(function (response) {
     for (i= 1; i < searchQty; i++){ 
-      // REMOVE ME! Testing log
-      console.log(response);
+      
       var newCard = $("<div class='card mt-4 mx-2 float-left'>")
       var newImage = $("<img class='card-img-top img-thumbnail'>");
-      // setting variables for the urls for the still image and the motion image
+
+      // Creating variables for the urls for the still images and animated images.
       var stillImage = response.data[i-1].images.downsized_still.url;
       var motionImage = response.data[i-1].images.downsized.url;
-      // setting the "alt" to whatever is provided from GIPHY for the title
+
+      // Adding the alternate naming attribute to the results provided from GIPHY for the title.
       $(newImage).attr("alt", response.data[i-1].title);
-      // set the default source to the still image
+
+      // Set the initial image source to the still image.
       $(newImage).attr("src", stillImage);
-      // set the still image and motion image urls as an attribute to easily reference with (this) later.
+
+      // Set the still image and animated image urls as an attribute to hook in a reference to.
       $(newImage.attr("img-still", stillImage));
       $(newImage.attr("img-motion", motionImage));
+
+      //Sets the state of each gif to still, by default.
       $(newImage).attr("current-state", "img-still");
+
+      //Genrates the new card and appends it to the list presented to the user.
       var newCardBody = $("<div class='card-body'>");
-      // var newCardTitle = $("<h5 class='card-title'>");
-      // newCardTitle.html("Title: " + response.data[i-1].title);
       var newCardText = $("<p class='card-text'>");
       newCardText.html("Rating: " + response.data[i-1].rating);
-      // newCardBody.append(newCardTitle).append(newCardText);
       newCardBody.append(newCardText);
       newCard.append(newImage).append(newCardBody);
       $("#animals").append(newCard);
     };
   })
 };
+
+//This function adds an animal button based on submitted user input.
 function addAnimal() {
   event.preventDefault();
   var newAnimal = $("#animal-input").val();
   topics.push(newAnimal);
   $("#animal-input").val("");
-  buttonMaker();
+  buttonGenerator();
 };
-// event listeners
+
+// These are the event listeners
 $(document).on('click', '.animal-button', cardMaker); 
-$(document).on('click', '.img-thumbnail', motionMaker);
+$(document).on('click', '.img-thumbnail', motionToggle);
 $(document).on('click', '#add-animal', addAnimal);
-// call the buttonMaker function to populate the page.
-buttonMaker();
+
+// Calls the buttonGenerator function to populate all animal buttons on HTML page.
+buttonGenerator();
